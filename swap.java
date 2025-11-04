4d701779d8d37fb9eaec1cb632514032edfb4920
rahul-gorantla <rahulgorantla07@gmail.com>
2025-11-04 11:04:06 +0530

    Initial-Commit

---
+++ swap.java
@@ -0,0 +1,10 @@
+class swap {
+	public static void main(String args[]){
+		int a=10,b=20,temp;
+		System.out.println("Beforing swapping a = "+a+" b = "+b);
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("After swapping a = "+a+" b = "+b);
+	}
+}
